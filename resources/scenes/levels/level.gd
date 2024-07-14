d7596003fcfecc766647c8bfa2f325068ac9cd8c
DocodeLee <167724096+DocodeLee@users.noreply.github.com>
2024-07-14 19:34:25 +0900

    laser, grenade and camera

--- resources/scenes/levels/level.gd
+++ resources/scenes/levels/level.gd
@@ -6,18 +6,21 @@ func _on_gate_player_entered_gate(body):
 	print("Player has enetered gate")
 	print(body)
 
-func _on_player_laser_fire(pos):
-	var laser = laser_scene.instantiate()
+func _on_player_laser_fire(pos, direction):
+	var laser = laser_scene.instantiate() as Area2D
 	#update the laser position
 	laser.position = pos
+	laser.rotation_degrees = rad_to_deg(direction.angle()) +90
+	laser.direction = direction
 	# want to add the laser instance to a Node2D
 	$Projectiles.add_child(laser)
 
 
-func _on_player_grenade_fire(pos):
-	var grenade = grenade_scene.instantiate()
+func _on_player_grenade_fire(pos, direction):
+	var grenade = grenade_scene.instantiate() as RigidBody2D
 	#grenade position
 	grenade.position = pos
+	grenade.linear_velocity = direction * grenade.speed
 	$Projectiles.add_child(grenade)
 	
 	
--- resources/scenes/player/player.gd
+++ resources/scenes/player/player.gd
@@ -4,8 +4,8 @@ var can_laser : bool = true
 var can_grenade : bool = true
 var speed = 450
 
-signal laser_fire(pos)
-signal grenade_fire(pos)
+signal laser_fire(pos, direction)
+signal grenade_fire(pos, direction)
 
 
 func _on_shot_timer_timeout():
@@ -21,15 +21,21 @@ func _process(_delta):
 	velocity = direction * speed
 	move_and_slide()
 	
+	#rotate
+	look_at(get_global_mouse_position())
+	
+	var player_direction = (get_global_mouse_position()- position).normalized()
 	if Input.is_action_pressed("primary action") and can_laser == true:
 		#randomly selected a marker 2D
 		var laser_markers = $LaserStartPosition.get_children()
 		# select random marker
 		var selected_laser = laser_markers[randi() % laser_markers.size()]
+		
 		can_laser = false
 		$ShotTimer.start()
+		
 		# emit the laser
-		laser_fire.emit(selected_laser.global_position)
+		laser_fire.emit(selected_laser.global_position, player_direction)
 		
 		
 	if Input.is_action_just_pressed("secondary action") and can_grenade == true:
@@ -39,7 +45,8 @@ func _process(_delta):
 		var selected_grenade = grenade_markers[randi() % grenade_markers.size()]
 		can_grenade = false
 		$GrenadeTimer.start()
-		grenade_fire.emit(selected_grenade.global_position)
+		
+		grenade_fire.emit(selected_grenade.global_position, player_direction)
 		
 
 
